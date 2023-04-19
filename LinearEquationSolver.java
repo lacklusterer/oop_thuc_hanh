2141f6a59a606a66ea174d85d0bc1a59d21a87d8
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-04-19 11:42:53 +0700

    2.2.6 Added handle for case where user input value 0 for a

--- LinearEquationSolver.java
+++ LinearEquationSolver.java
@@ -1,11 +1,25 @@
 import javax.swing.JOptionPane;
+import java.util.Objects;
 
 public class LinearEquationSolver {
     public static void main(String[] args) {
 
-        String aString = JOptionPane.showInputDialog(null,
-                "ax + b = 0\nEnter the value of a:", "LinearEquationSolver",
-                JOptionPane.INFORMATION_MESSAGE);
+        String aString = "";
+        boolean isValid = false;
+        while (!isValid) {
+            try {
+                aString = JOptionPane.showInputDialog(null,
+                        "ax + b = 0\nEnter the value of a:", "LinearEquationSolver",
+                        JOptionPane.INFORMATION_MESSAGE);
+                if (Objects.equals(aString, "0")) {
+                    throw new Exception("Value of a cannot be zero!");
+                }
+                isValid = true;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error: " + e.getMessage(),
+                        "Invalid Input", JOptionPane.ERROR_MESSAGE);
+            }
+        }
         String bString = JOptionPane.showInputDialog(null,
                 String.format("%sx + b = 0\nEnter the value of b:", aString), "LinearEquationSolver",
                 JOptionPane.INFORMATION_MESSAGE);
