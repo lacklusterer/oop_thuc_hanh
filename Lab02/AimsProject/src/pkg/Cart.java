7e7bb6fd879a1d03bdfbf2a6c9fe5bb3b1b60482
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-05-17 18:35:15 +0700

    Added Cart.java

---
+++ Lab02/AimsProject/src/pkg/Cart.java
@@ -0,0 +1,33 @@
+package pkg;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] =
+            new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered = 0;
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered++] = disc;
+        } else {
+            System.out.println("Cart is full!");
+        }
+    }
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for (int i = 0; i < itemsOrdered.length; i++) {
+            if (itemsOrdered[i] == disc) {
+                qtyOrdered--;
+                itemsOrdered[i] = null;
+                break;
+            } else {
+                System.out.printf("%s is not in cart%n", disc);
+            }
+        }
+    }
+    public float totalCost() {
+        float total = 0.0f;
+        for (int i = 0; i <= qtyOrdered; i++) {
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+}
