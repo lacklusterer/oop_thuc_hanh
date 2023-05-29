ae6531e5652e271badc65af95776e2c015136ed3
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-05-29 14:44:51 +0700

    Implemented swapWith() method to swap two objects using newly defined wrapper class

--- AimsProject/src/pkg/TestPassingParameter.java
+++ AimsProject/src/pkg/TestPassingParameter.java
@@ -5,14 +5,27 @@ public class TestPassingParameter {
         DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
         DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
 
+        //Print name of created dvd
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
-        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle() + "\n");
+
+        //Swap pass-by-value
         swap(jungleDVD, cinderellaDVD);
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
-        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle() + "\n");
 
-        changeTitle(jungleDVD, cinderellaDVD.getTitle());
-        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        //Change title
+        //changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        //System.out.println("jungle dvd title: " + jungleDVD.getTitle() + "\n");
+
+        //Create wrapper to wrap the created dvd
+        DvdWrapper wrappedJungleDVD = new DvdWrapper(jungleDVD);
+        DvdWrapper wrappedCinderellaDVD = new DvdWrapper(cinderellaDVD);
+
+        //Swap the wrapped dvd
+        wrappedJungleDVD.swapWith(wrappedCinderellaDVD);
+        System.out.println("jungle dvd title: " + wrappedJungleDVD.dvd.getTitle());
+        System.out.println("cinderella dvd title: " + wrappedCinderellaDVD.dvd.getTitle() + "\n");
     }
     public static void swap(Object o1, Object o2) {
         Object tmp = o1;
@@ -25,5 +38,17 @@ public class TestPassingParameter {
         dvd.setTitle(title);
         dvd = new DigitalVideoDisc(oldTitle);
         System.out.printf("Changed title from %s to %s\n", oldTitle, title);
+    }
+}
+class DvdWrapper {
+    public DigitalVideoDisc dvd;
+    public DvdWrapper(DigitalVideoDisc dvd) {
+        this.dvd = dvd;
+    }
+    public void swapWith(DvdWrapper swapper) {
+        DigitalVideoDisc tmp = dvd;
+        dvd = swapper.dvd;
+        swapper.dvd = tmp;
+        System.out.printf("Swapped %s with %s using wrapper class" + "\n", dvd.getTitle(), swapper.dvd.getTitle());
     }
 }
