256c9e3b5a88909fd9bf1703a840962b6e90e3d8
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-06-07 01:07:27 +0700

    topic/memory-management-string

---
+++ OtherProjects/hust/soict/cs01/garbage/GarbageCreator.java
@@ -0,0 +1,22 @@
+package OtherProjects.hust.soict.cs01.garbage;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GarbageCreator {
+	public static void main(String[] args) {
+		var filename = "y.png";
+
+		byte[] inputBytes = { 0 };
+		try {
+			inputBytes = Files.readAllBytes(Paths.get(filename));
+		} catch (Exception e) {}
+		var start = System.currentTimeMillis();
+
+		var output = "";
+		for (byte b: inputBytes) {
+			output += (char)b;
+		}
+		System.out.println(System.currentTimeMillis() - start);
+	}
+}
