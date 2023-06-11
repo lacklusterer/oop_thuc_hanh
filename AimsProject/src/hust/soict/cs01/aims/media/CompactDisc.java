1cfbb92f0df3d57fef5f20076f682dd470bb6cf2
lasklusterer <minh.nn210582@sis.hust.edu.vn>
2023-06-11 23:08:50 +0700

    Created subclass CompactDisc to Disc

---
+++ AimsProject/src/hust/soict/cs01/aims/media/CompactDisc.java
@@ -0,0 +1,7 @@
+package hust.soict.cs01.aims.media;
+
+public class CompactDisc extends Disc{
+    public CompactDisc(String title, String category, float cost, int length, String director) {
+        super(title, category, cost, length, director);
+    }
+}
