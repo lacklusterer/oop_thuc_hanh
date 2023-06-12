f992ec182c18a1251f6cabbe2d3a56d986f9785e
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-06-12 13:46:32 +0700

    Simple implementation of addMedia, removeMedia and totalCost Removed old Cart methods

--- AimsProject/src/hust/soict/cs01/aims/cart/Cart.java
+++ AimsProject/src/hust/soict/cs01/aims/cart/Cart.java
@@ -7,48 +7,20 @@ import java.util.ArrayList;
 public class Cart {
 
     private ArrayList<Media> itemsOrdered = new ArrayList<Media>();
-    /*
+
+    public void addMedia(Media media) {
+        itemsOrdered.add(media);
+    }
+
+    public void removeMedia(Media media) {
+        itemsOrdered.remove(media);
+    }
+
     public float totalCost() {
         float total = 0.0f;
-        for (int i = 0; i < qtyOrdered; i++) {
-            total += itemsOrdered[i].getCost();
+        for (Media media : itemsOrdered) {
+            total += media.getCost();
         }
         return total;
     }
-    // print method
-    public void print() {
-        System.out.printf("""
-                ***********************CART***********************\s
-                Ordered Items:
-                """);
-        for (int i = 0; i < qtyOrdered; i++) {
-            System.out.printf("%d. %s\n", i + 1, itemsOrdered[i].toString());
-        }
-        System.out.printf("""
-                Total cost: %.2f$
-                ***************************************************
-                """, totalCost());
-    }
-
-    // Search methods
-    public void searchID(int id) {
-        for (int i = 0; i < qtyOrdered; i++) {
-            if (itemsOrdered[i].getId() == id) {
-                System.out.println(itemsOrdered[i]);
-                return;
-            }
-        }
-        System.out.println("No match!");
-    }
-
-    public void searchTitle(String title) {
-        for (int i = 0; i < qtyOrdered; i++) {
-            if (itemsOrdered[i].isMatch(title)) {
-                System.out.println(itemsOrdered[i]);
-                return;
-            }
-        }
-        System.out.println("No match!");
-    }
-    */
 }
