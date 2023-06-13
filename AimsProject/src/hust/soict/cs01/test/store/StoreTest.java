d173555fbb05b710bb6eac78000d5bc255607db0
lasklusterer <minh.nn210582@sis.hust.edu.vn>
2023-06-13 23:58:02 +0700

    Tested Polymorphism with toString() method inside Aims.java

--- AimsProject/src/hust/soict/cs01/test/store/StoreTest.java
+++ AimsProject/src/hust/soict/cs01/test/store/StoreTest.java
@@ -11,7 +11,7 @@ public class StoreTest {
 
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("Obama: The Movie",
                 "Comedy", 29.99f, 180, "Michael Bay");
-        Store1.addDVD(dvd1);
-        Store1.removeDVD(dvd1);
+        Store1.addMedia(dvd1);
+        Store1.addMedia(dvd1);
     }
 }
