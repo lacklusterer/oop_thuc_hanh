3ce3849438733549f519335e1207a2118627afeb
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-06-15 23:25:18 +0700

    topic/aims-console: Reorganized and added back deleted codes for upcoming console implementation

--- AimsProject/src/hust/soict/cs01/aims/cart/Cart.java
+++ AimsProject/src/hust/soict/cs01/aims/cart/Cart.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class Cart {
 
     public static final int MAX_NUMBERS_ORDERED = 20;
-    private ArrayList<Media> itemsOrdered = new ArrayList<Media>();
+    private ArrayList<Media> itemsOrdered = new ArrayList<>();
 
     public void addMedia(Media media) {
         if (itemsOrdered.size() < MAX_NUMBERS_ORDERED) {
@@ -33,5 +33,42 @@ public class Cart {
             total += media.getCost();
         }
         return total;
+    }
+
+    // Search methods
+    public void searchID(int id) {
+        for (Media item : itemsOrdered) {
+            if (item.getId() == id) {
+                System.out.println(item);
+                return;
+            }
+        }
+        System.out.println("No match!");
+    }
+
+    public void searchTitle(String title) {
+        for (Media item : itemsOrdered) {
+            if (item.isMatch(title)) {
+                System.out.println(item);
+                return;
+            }
+        }
+        System.out.println("No match!");
+    }
+
+    public void print() {
+        System.out.print("""
+                ***********************CART***********************\s
+                Ordered Items:
+                """);
+        int i = 0;
+        for (Media item : itemsOrdered) {
+            System.out.printf("%d. %s\n", i + 1, item.toString());
+            i++;
+        }
+        System.out.printf("""
+                Total cost: %.2f$
+                ***************************************************
+                """, totalCost());
     }
 }
--- AimsProject/src/hust/soict/cs01/test/cart/CartTest.java
+++ AimsProject/src/hust/soict/cs01/test/cart/CartTest.java
@@ -1,35 +1,53 @@
 package hust.soict.cs01.test.cart;
 
+import hust.soict.cs01.aims.media.Book;
+import hust.soict.cs01.aims.media.CompactDisc;
 import hust.soict.cs01.aims.media.DigitalVideoDisc;
 import hust.soict.cs01.aims.cart.Cart;
+import hust.soict.cs01.aims.media.Media;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CartTest {
     public static void main(String[] args) {
-        //Create cart named anOrder
         Cart anOrder = new Cart();
 
         //New dvd, add to cart
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Obama: The Movie",
-                "Comedy", 29.99f, 180, "Michael Bay");
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Obama: The Movie", "Comedy", 29.99f, 180, "Michael Bay");
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Morbius 2: Morbing time", "Action Comedy", 39.99f, 165, "Daniel Espinosa");
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 19.99f);
         anOrder.addMedia(dvd1);
-
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Morbius 2: Morbing time",
-                "Action Comedy", 39.99f, 165, "Daniel Espinosa");
         anOrder.addMedia(dvd2);
-
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
-                "Animation", 19.99f);
         anOrder.addMedia(dvd3);
 
-        /*
-        // Print method
+        //Get total cost
+        System.out.printf("Total cost is %.2f\n", anOrder.totalCost());
+
+        // Print method test
         anOrder.print();
 
-        //Search method
+        //Search method test
         anOrder.searchID(2);
         anOrder.searchTitle("Morbius 2: Morbing time");
         anOrder.searchID(5);
         anOrder.searchTitle("Aladiin");
-        */
+
+
+        // Sort comparator test
+        List<Media> mediaList = new ArrayList<>();
+
+        Book book1 = new Book("BookTitle", "BookCategory", 19.99f);
+        CompactDisc cd1 = new CompactDisc("CDNuts", "CDCategory", 39.99f, "NaN");
+
+        mediaList.add(book1);
+        mediaList.add(cd1);
+        mediaList.add(dvd1);
+
+        mediaList.sort(Media.COMPARE_BY_COST_TITLE);
+
+        for (Media media : mediaList) {
+            System.out.println(media.toString());
+        }
     }
 }
