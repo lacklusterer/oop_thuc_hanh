9a2b5c02b1b5459df2e5a58e9d52985bed4b8c3c
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-06-27 22:05:24 +0700

    feature/aims-gui: added createNorth

--- AimsProject/src/hust/soict/cs01/aims/screen/StoreScreen.java
+++ AimsProject/src/hust/soict/cs01/aims/screen/StoreScreen.java
@@ -6,4 +6,12 @@ import javax.swing.*;
 
 public class StoreScreen extends JFrame {
     private Store store;
+    
+    JPanel createNorth() {
+        JPanel north = new JPanel();
+        north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
+        // north.add(createMenuBar());
+        // north.add(createHeader());
+        return north;
+    }
 }
