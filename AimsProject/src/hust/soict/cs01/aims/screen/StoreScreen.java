ca1f6b4fe315b5cc61ca63ae6f5402a54904e27f
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-06-27 22:02:07 +0700

    feature/aims-gui: initial commit

---
+++ AimsProject/src/hust/soict/cs01/aims/screen/StoreScreen.java
@@ -0,0 +1,9 @@
+package hust.soict.cs01.aims.screen;
+
+import hust.soict.cs01.aims.mediaconcontainer.store.Store;
+
+import javax.swing.*;
+
+public class StoreScreen extends JFrame {
+    private Store store;
+}
