6f5afbbb4ba6886c66ebcd2d75ffb3ad08b96acb
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-06-27 22:10:08 +0700

    feature/aims-gui: added createMenuBar

--- AimsProject/src/hust/soict/cs01/aims/screen/StoreScreen.java
+++ AimsProject/src/hust/soict/cs01/aims/screen/StoreScreen.java
@@ -3,6 +3,7 @@ package hust.soict.cs01.aims.screen;
 import hust.soict.cs01.aims.mediaconcontainer.store.Store;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class StoreScreen extends JFrame {
     private Store store;
@@ -10,8 +11,28 @@ public class StoreScreen extends JFrame {
     JPanel createNorth() {
         JPanel north = new JPanel();
         north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
-        // north.add(createMenuBar());
+        north.add(createMenuBar());
         // north.add(createHeader());
         return north;
+    }
+
+    JMenuBar createMenuBar() {
+
+        JMenu menu = new JMenu("Options");
+
+        JMenu smUpdateStore = new JMenu("Update Store");
+        smUpdateStore.add(new JMenuItem("Add Book"));
+        smUpdateStore.add(new JMenuItem("Add CD"));
+        smUpdateStore.add(new JMenuItem("Add DVD"));
+
+        menu.add(smUpdateStore);
+        menu.add(new JMenuItem("View store"));
+        menu.add(new JMenuItem("View cart"));
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+        menuBar.add(menu);
+
+        return menuBar;
     }
 }
