9f316cb7423ee7accf4b05fad5328790a34ed272
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-06-28 11:49:50 +0700

    feature/aims-gui: added functionality for "Play" button

--- AimsProject/src/hust/soict/cs01/aims/screen/MediaStore.java
+++ AimsProject/src/hust/soict/cs01/aims/screen/MediaStore.java
@@ -1,10 +1,12 @@
 package hust.soict.cs01.aims.screen;
 
 import javax.swing.*;
-import hust.soict.cs01.aims.media.Media;
-import hust.soict.cs01.aims.media.Playable;
+
+import hust.soict.cs01.aims.media.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MediaStore extends JPanel {
     private Media media;
@@ -24,9 +26,39 @@ public class MediaStore extends JPanel {
         container.setLayout(new FlowLayout(FlowLayout.CENTER));
 
         container.add(new JButton("Add to cart"));
+
         if (media instanceof Playable) {
-            container.add(new JButton("Play"));
+            JButton playButton = new JButton("Play");
+            playButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // create a dialog Box
+                    JDialog playDialog = new JDialog(new JFrame(), media.getTitle());
+                    JLabel label;
+
+                    if (media instanceof DigitalVideoDisc) {
+                        DigitalVideoDisc dvd = (DigitalVideoDisc) media;
+                        label = new JLabel("Playing DVD: " + dvd.getTitle() + "\n" + "DVD length: " + dvd.getLength());
+                    } else {
+                        CompactDisc cd = (CompactDisc) media;
+                        StringBuilder trackPlay = new StringBuilder();
+                        for (Track track : cd.getTracksList()) {
+                            trackPlay.append("Playing Track: " + track.getTitle() + "\n" + "Track length: " + track.getLength()+"\n");
+                        }
+                        label = new JLabel(String.valueOf(trackPlay));
+                    }
+
+                    playDialog.add(label);
+
+                    playDialog.setSize(450, 450);
+
+                    playDialog.setVisible(true);
+                }
+            });
+            container.add(playButton);
         }
+
 
         this.add(Box.createVerticalGlue());
         this.add(title);
@@ -36,4 +68,6 @@ public class MediaStore extends JPanel {
 
         this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
     }
+
+
 }
