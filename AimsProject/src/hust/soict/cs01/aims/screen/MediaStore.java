ad09f8022bd9e046085f3544c3e6873c29626660
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-06-28 10:46:22 +0700

    feature/aims-gui: added MediaStore class

---
+++ AimsProject/src/hust/soict/cs01/aims/screen/MediaStore.java
@@ -0,0 +1,39 @@
+package hust.soict.cs01.aims.screen;
+
+import javax.swing.*;
+import hust.soict.cs01.aims.media.Media;
+import hust.soict.cs01.aims.media.Playable;
+
+import java.awt.*;
+
+public class MediaStore extends JPanel {
+    private Media media;
+    public MediaStore(Media media) {
+
+        this.media = media;
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JLabel title = new JLabel(media.getTitle());
+        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 20));
+        title.setAlignmentX(CENTER_ALIGNMENT);
+
+        JLabel cost = new JLabel("$"+ media.getCost());
+        cost.setAlignmentX(CENTER_ALIGNMENT);
+
+        JPanel container = new JPanel();
+        container.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        container.add(new JButton("Add to cart"));
+        if (media instanceof Playable) {
+            container.add(new JButton("Play"));
+        }
+
+        this.add(Box.createVerticalGlue());
+        this.add(title);
+        this.add(cost);
+        this.add(Box.createVerticalGlue());
+        this.add(container);
+
+        this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
+}
