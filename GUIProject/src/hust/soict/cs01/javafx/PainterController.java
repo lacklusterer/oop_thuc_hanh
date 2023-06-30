cb6eec63294639d022ca63f09c36b8d463542337
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-06-30 23:02:01 +0700

    topic/painter: Added Eraser feature

--- GUIProject/src/hust/soict/cs01/javafx/PainterController.java
+++ GUIProject/src/hust/soict/cs01/javafx/PainterController.java
@@ -2,6 +2,8 @@ package hust.soict.cs01.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -10,7 +12,12 @@ import javafx.scene.shape.Circle;
 public class PainterController {
 
     @FXML
+    private ToggleGroup Tool;
+
+    @FXML
     private Pane drawingAreaPane;
+
+    private Color drawColor = Color.BLACK; // Default pen color
 
     @FXML
     void clearButtonPressed(ActionEvent event) {
@@ -19,8 +26,17 @@ public class PainterController {
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
+        Circle newCircle = new Circle(event.getX(), event.getY(), 4, drawColor);
         drawingAreaPane.getChildren().add(newCircle);
     }
 
+    @FXML
+    void handleColorRadioButton(ActionEvent event) {
+        RadioButton selectedRadioButton = (RadioButton) Tool.getSelectedToggle();
+        if (selectedRadioButton.getText().equals("Pen")) {
+            drawColor = Color.BLACK;
+        } else if (selectedRadioButton.getText().equals("Eraser")) {
+            drawColor = Color.WHITE;
+        }
+    }
 }
