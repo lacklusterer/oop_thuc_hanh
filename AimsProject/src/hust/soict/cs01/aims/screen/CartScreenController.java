2c00e775526d85e6b60c5afabf017c79311af0c4
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-07-01 22:13:07 +0700

    topic/aims-javafx-integration: created CartScreen and stub for the Controller

---
+++ AimsProject/src/hust/soict/cs01/aims/screen/CartScreenController.java
@@ -0,0 +1,8 @@
+package hust.soict.cs01.aims.screen;
+
+import hust.soict.cs01.aims.mediaconcontainer.cart.Cart;
+
+public class CartScreenController {
+    public CartScreenController(Cart cart) {
+    }
+}
