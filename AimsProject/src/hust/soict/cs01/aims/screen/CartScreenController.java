0aa8cfeb6de95a1c82da2a165df25d54e4ec33fb
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-07-02 10:52:15 +0700

    topic/aims-javafx-integration: Play button visibility is now based on Media

--- AimsProject/src/hust/soict/cs01/aims/screen/CartScreenController.java
+++ AimsProject/src/hust/soict/cs01/aims/screen/CartScreenController.java
@@ -1,11 +1,18 @@
 package hust.soict.cs01.aims.screen;
 
 import hust.soict.cs01.aims.media.Media;
+import hust.soict.cs01.aims.media.Playable;
 import hust.soict.cs01.aims.mediaconcontainer.cart.Cart;
+import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Button;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+import javax.swing.event.ChangeEvent;
 
 public class CartScreenController {
 
@@ -15,6 +22,12 @@ public class CartScreenController {
         super();
         this.cart = cart;
     }
+
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnRemove;
 
     @FXML
     private TableColumn<Media, Float> colMediaCost;
@@ -30,12 +43,31 @@ public class CartScreenController {
 
     @FXML
     private void initialize() {
-        colMediaTitle.setCellValueFactory(
-                new PropertyValueFactory<Media, String>("title"));
-        colMediacategory.setCellValueFactory(
-                new PropertyValueFactory<Media, String>("category"));
-        colMediaCost.setCellValueFactory(
-                new PropertyValueFactory<Media, Float>("cost"));
+        colMediaTitle.setCellValueFactory(new PropertyValueFactory<Media, String>("title"));
+        colMediacategory.setCellValueFactory(new PropertyValueFactory<Media, String>("category"));
+        colMediaCost.setCellValueFactory(new PropertyValueFactory<Media, Float>("cost"));
         tblMedia.setItems(this.cart.getItemsOrdered());
+
+        btnPlay.setVisible(false);
+        btnRemove.setVisible(false);
+
+        tblMedia.getSelectionModel().selectedItemProperty().addListener(
+                new ChangeListener<Media>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Media> observable, Media oldValue, Media newValue) {
+                        if(newValue != null) {
+                            updateButtonBar(newValue);
+                        }
+                    }
+                });
+
+        }
+    void updateButtonBar(Media media) {
+        btnRemove.setVisible(true);
+        if(media instanceof Playable) {
+            btnPlay.setVisible(true);
+        } else {
+            btnPlay.setVisible(false);
+        }
     }
 }
